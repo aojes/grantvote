b089c81ab02fb5e09c291b194fe6cb9f8259dfc6
Katherine Giron Pe <info@bridgeutopiaweb.com>
2009-05-09 15:34:22 +0800

    Removed some files for gitignore to work

--- app/helpers/grants_helper.rb
+++
@@ -1,5 +0,0 @@
-module GrantsHelper
-  def voted?(grant)
-    Vote.exists?(:grant_id => grant.id, :user_id => current_user.id)
-  end
-end
--- features/step_definitions/watch_group_steps.rb
+++
@@ -1,9 +0,0 @@
-When /^I join a group without paying dues$/ do
-  @group.memberships.create!(:user => @user, :interest => false)
-end
-
-Then /^I am a member without voting interest$/ do
-  @group.memberships.count.should == 1
-  @group.memberships.voters.count.should == 0
-end
-
