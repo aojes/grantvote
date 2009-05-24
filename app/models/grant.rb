e0fead1f96e7c8ae2288bafde5332d85627f4d31
Katherine Giron Pe <info@bridgeutopiaweb.com>
2009-05-24 21:10:44 +0800

    Fixed the problem on nil.photo. Simple condition on if group header partial. Also added paperclip validations. Do modify if needed. Merging changes to master branch in a while

--- app/models/grant.rb
+++ app/models/grant.rb
@@ -40,7 +40,9 @@ class Grant < ActiveRecord::Base
     :message => "can be an integer value greater than or equal to $#{MIN_AWARD}"
   
   # TODO
-  # validates_attachment_presence :photo
+  # validates_attachment_presence :photo 
+  validates_attachment_size :photo, :less_than=> 600.kilobytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpg']
   
   before_save :adapt_objects
   # before_save :adapt_links
