ad934af24126a5da00b34a4b4bc01e0941e91c34
Jesse Crockett <jess@home.(none)>
2009-06-01 16:36:56 -0500

    moonshine time

---
+++ config/_deploy.rb
@@ -0,0 +1,37 @@
+default_run_options[:pty] = true
+
+ssh_options[:forward_agent] = true
+
+
+# be sure to change these
+set :user, 'root'
+set :domain, '67.23.26.17'
+set :application, 'grantvote'
+set :port, 3288
+
+set :repository,  "git@github.com:tefflox/grantvote.git" 
+set :deploy_to, "/var/web/staging/" 
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :branch, 'staging'
+set :git_shallow_clone, 1
+set :git_enable_submodules, 1
+set :scm_verbose, true
+set :use_sudo, false
+
+server domain, :app, :web
+role :db, domain, :primary => true
+
+namespace :deploy do
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt" 
+  end
+  
+  task :symlink_shared do
+    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"  
+  end 
+end
+
+after 'deploy:update_code', 'deploy:symlink_shared'
+
+
--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   
   config.gem "searchlogic"
+  config.gem "paperclip"
   config.gem "populator"
   config.gem "faker"
   config.gem "haml"
