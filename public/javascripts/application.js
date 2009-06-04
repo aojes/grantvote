3c9149e26594cb90e663591431838debc783c7a3
Katherine Giron Pe <info@bridgeutopiaweb.com>
2009-06-05 04:06:57 +0800

    Round corners now flexible width. To do groups view and account view

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,19 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-
-$(document).ready( function(){
-  $('.rounded').corners("3px");
-  $('div.pagination a').corners("4px");
-  $('.pagination').corners("6px");
-});
-
 $.fn.pause = function(duration) {
     $(this).animate({ dummy: 1 }, duration);
     return this;
 };
 
+
 $(document).ready( function(){
-  $('#flash-notice').grow().highlight().pause(9000).shrink();
+  $('.rounded').corners("3px");
+  $('div.pagination a').corners("4px");
+  $('#errorExplanation').corners("6px");
+  $('.pagination').corners("6px");
+   $('#flash-notice').grow().highlight().pause(9000).shrink();
   $('#flash-warning').grow().highlight().pause(9000).shrink();
   $('#flash-error').grow().highlight().pause(9000).shrink();
 });
