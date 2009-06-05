7d8cf838a8b5f970b3853de8ae9125f816850c83
Jesse Crockett <jess@home.(none)>
2009-06-05 08:46:43 -0500

    make Grants portal

--- app/controllers/grants_controller.rb
+++ app/controllers/grants_controller.rb
@@ -8,24 +8,48 @@ class GrantsController < ApplicationController
   def index
     if params[:group_id]
 
-     @group = Group.find_by_permalink(params[:group_id])
-     @page_title = "Voting Session for " + @group.name
+      @group = Group.find_by_permalink(params[:group_id])
+      @page_title = "Voting Session for " + @group.name
 
-     @search = @group.grants.new_search(params[:search])
+      @search = @group.grants.new_search(params[:search])
 
-     @search.conditions.final = false
-     @search.per_page = 10
-     @search.order_as = "ASC"
-     @search.order_by = :created_at
+      @search.conditions.final = false
+      @search.per_page = 10
+      @search.order_as = "ASC"
+      @search.order_by = :created_at
     
-     @grants = @search.all
-     @grants_count = @search.count                          
+      @grants = @search.all
+      @grants_count = @search.count                          
                           
     elsif params[:user_id]
       @grants = Grant.find_all_by_user_id(params[:user_id], 
                                             :order => "created_at ASC")
       @page_title = "Listing Grants"                                            
+    else
+      @page_title = "Grants on Grantvote"
+
+      @search = Grant.new_search(params[:search])
+      
+      if params[:search]
+        if params[:search][:conditions]
+          query = params[:search][:conditions][:proposal_keywords].split
+        
+          @search.conditions.or_group do |g|
+            g.proposal_keywords = query
+          end 
+        end
+      end  
+      @search.per_page = 10
+      @search.order_by, @search.order_as = [:updated_at], 'DESC'
+      @search.conditions.final = true
+      @search.conditions.awarded = true
+      @search.per_page = 10
+
+      @grants, @grants_count = @search.all, @search.count
     end
+    respond_to do |format|
+      format.html # index.html.erb
+    end    
   end
   
   def show
