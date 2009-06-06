36742f51406eb5666fd0b87485b9b324e08a97ca
Jesse Crockett <jess@home.(none)>
2009-06-06 01:03:24 -0500

    make group page nicer, plot areas

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,8 +10,11 @@ $(document).ready( function(){
   $('.rounded').corners("3px");
   $('div.pagination a').corners("4px");
   $('#errorExplanation').corners("6px");
+  $('.success').corners("6px");
+  $('.notice').corners("6px");
+  $('.error').corners("6px");
   $('.pagination').corners("6px");
-   $('#flash-notice').grow().highlight().pause(9000).shrink();
+  $('#flash-notice').grow().highlight().pause(9000).shrink();
   $('#flash-warning').grow().highlight().pause(9000).shrink();
   $('#flash-error').grow().highlight().pause(9000).shrink();
 });
