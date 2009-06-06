5f98d76ef1ae9e9b5864f1ae4e1f9c9567670443
Jesse Crockett <jess@home.(none)>
2009-06-05 22:22:02 -0500

    some "progress" on social functions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,6 +4,8 @@ class ProfilesController < ApplicationController
   def view
     @user = User.find_by_login(params[:permalink])
     @page_title = @user.login + " on Grantvote"
+    
+   
     respond_to do |format|
       if @user
         format.html
