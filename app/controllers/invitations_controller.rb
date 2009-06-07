bcb4b8a1534d9da22ffcee3fbea46a95bdc0bd77
Jesse Crockett <jess@home.(none)>
2009-06-07 06:41:55 -0500

    lite cleanup

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,19 +4,16 @@ class InvitationsController < ApplicationController
   
   def index
     if current_user 
-    # @page_title ="Manage Invitations"
-    # @invitations = Invitation.find(:all)
+      # @page_title ="Manage Invitations"
+      # @invitations = Invitation.find(:all)
         
-    respond_to do |format|
-     # format.html { redirect_to(invitations_url) }
-     format.html { redirect_to profile_path(current_user.login)  }
-    end
-    
+      respond_to do |format|
+        # format.html { redirect_to(invitations_url) }
+        format.html { redirect_to profile_path(current_user.login)  }
+      end
     else
-    @page_title = "Grantvote"
-
+      @page_title = "Grantvote"
     end
-    
   end
   
 
@@ -42,34 +39,31 @@ class InvitationsController < ApplicationController
         
         redirect_to root_url
       end
-
     elsif current_user
        redirect_to root_url
     else
       render :action => :new
     end
-    
   end
   
   def approve_invitations
   
     if current_user.id == 1
-    @invitation = Invitation.find(:all)
+      @invitation = Invitation.find(:all)
     else
-    redirect_to root_url
+      redirect_to root_url
     end
-  
   end
   
-
   def send_invite
-     @invitation  = Invitation.find(params[:id])
+    @invitation  = Invitation.find(params[:id])
 
-     @invitation.sender = current_user
-     Mailer.deliver_invitation(@invitation, signup_url(@invitation.token))
+    @invitation.sender = current_user
+    Mailer.deliver_invitation(@invitation, signup_url(@invitation.token))
+    
     respond_to do |format|
-     # format.html { redirect_to(invitations_url) }
-     format.html { redirect_to profile_path(current_user.login)  }
+      # format.html { redirect_to(invitations_url) }
+      format.html { redirect_to profile_path(current_user.login)  }
     end
   end
   
@@ -78,15 +72,14 @@ class InvitationsController < ApplicationController
   def self.send_all_invites
     
     Invitation.find(:all).each do | i |
-     if i.sender_id.nil?
-     @invitation = i
-     @invitation.sender = current_user
-     #i.update_attribute(:sender_id, 1)
-     Mailer.deliver_invitation(@invitation, signup_url(@invitation.token))
-     end
+      if i.sender_id.nil?
+        @invitation = i
+        @invitation.sender = current_user
+        
+        #i.update_attribute(:sender_id, 1)
+        Mailer.deliver_invitation(@invitation, signup_url(@invitation.token))
+      end
     end
-  
   end
-  
   
 end
