1cc326ca51ebbcd395a8dd0dc25cba8f431901e3
Jesse Crockett <jess@home.(none)>
2009-06-11 07:54:56 -0500

    specify searchlogic version 1.6

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   
-  config.gem "searchlogic"
+  config.gem "searchlogic", :version => "~> 1.6.6"
   config.gem "populator"
   config.gem "faker"
   config.gem "haml"
