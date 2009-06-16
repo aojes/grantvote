46f201ec9bec2669f54a86e33f6751a4d5ae47d5
Jesse Crockett <jess@home.(none)>
2009-06-16 01:20:04 -0500

    add Grants Award Total

--- app/helpers/grants_helper.rb
+++ app/helpers/grants_helper.rb
@@ -1,5 +1,11 @@
 module GrantsHelper
+  
   def voted?(grant)
     Vote.exists?(:grant_id => grant.id, :user_id => current_user.id)
   end
+  
+  def award_total
+    Grant.find_all_by_awarded(true).collect {|g| g.amount}.sum
+  end
+  
 end
