57a0ddb17fa8887c503f5a5a94009b25810d53f5
Jesse Crockett <jess@home.(none)>
2009-06-16 08:36:53 -0500

    patch voting checks

--- app/helpers/blitzes_helper.rb
+++ app/helpers/blitzes_helper.rb
@@ -1,5 +1,9 @@
 module BlitzesHelper
  
+  def voting?(blitz)
+    Vote.exists?(:blitz_id => blitz, :user_id => current_user).nil?
+  end
+ 
   def show_blitz?(blitz)
     blitz.votes.count > 0 and 
     not current_user == blitz.user and 
