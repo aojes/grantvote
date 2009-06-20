0bf11b9fb096082bf4fce7f681ca3edec34bf220
Jesse Crockett <jess@home.(none)>
2009-06-20 07:01:08 -0500

    patch "thank you for voting" on blitzes

--- app/helpers/blitzes_helper.rb
+++ app/helpers/blitzes_helper.rb
@@ -3,7 +3,11 @@ module BlitzesHelper
   def voting_blitz?(blitz)
     not Vote.exists?(:blitz_id => blitz, :user_id => current_user)
   end
- 
+  
+  def voted_blitz?(blitz)
+    Vote.exists?(:blitz_id => blitz.id, :user_id => current_user.id)
+  end
+  
   def show_blitz?(blitz)
     blitz.votes.count > 0 and 
     not current_user == blitz.user and 
