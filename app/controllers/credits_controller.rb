fd83d8259d3b24d2b56b62bf64d4799923dd248d
Jesse Crockett <jess@home.(none)>
2009-06-20 10:55:07 -0500

    allow giving of beads for shells

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -7,4 +7,20 @@ class CreditsController < ApplicationController
     
   end
   
+  def update
+    @receives = Credit.new(params[:credit])
+    # defer model method for more complex scenarios
+    if current_user.credit.update_attributes(
+                               :beads   => (current_user.credit.beads - 1),
+                               :shells  => (current_user.credit.shells + 1) ) &&
+       @receives.user.credit.update_attributes(                         
+                               :beads   => (@receives.user.credit.beads + 1) )
+                                          
+      flash[:notice] = 'Success! Helping your fellow Grantvoter.'
+      redirect_back_or_default :back
+    else
+      flash[:notice] = 'Bleep, bloop.  Please try again.'
+      redirect_back_or_default :back
+    end
+  end  
 end
--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,10 +5,10 @@ class FriendshipsController < ApplicationController
     @friendship = current_user.friendships.
                                build(:friend_id => params[:friend_id])
     if @friendship.save
-      flash[:notice] = "Added friendship"
+      flash[:notice] = 'Added friendship!'
       redirect_back_or_default :back
     else
-      flash[:error] = "Bleep, bloop. System error. Please try again."
+      flash[:error] = 'Bleep, bloop. System error. Please try again.'
       redirect_back_or_default :back
     end
   end
@@ -16,8 +16,8 @@ class FriendshipsController < ApplicationController
   def destroy
     @friendship = current_user.friendships.find_by_friend_id(params[:id])
     @friendship.destroy
-    flash[:notice] = "Removed friendship"
-    # !
+    flash[:notice] = 'Removed friendship'
+    
     redirect_to profile_path(current_user.login)
   end
 
