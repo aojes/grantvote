1623a95c907559b90cba8091e1f2d0cbac3b66b2
Jesse Crockett <jess@home.(none)>
2009-08-02 12:57:21 -0500

    reset cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,20 +1,25 @@
 # Sets up the Rails environment for Cucumber
-ENV["RAILS_ENV"] ||= "test"
+ENV["RAILS_ENV"] ||= "cucumber"
 require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
-require File.expand_path(File.dirname(__FILE__) + "../../../spec/blueprints")
-#require File.expand_path(File.dirname(__FILE__) + "../../../vendor/gems/authlogic-2.0.9/test/test_helper"
-#require File.expand_path(File.dirname(__FILE__) + "../../../vendor/gems/authlogic-2.0.9/lib/authlogic/test_case")
-
 require 'cucumber/rails/world'
-require 'cucumber/formatters/unicode' # Comment out this line if you don't want Cucumber Unicode support
+
+# Comment out the next line if you don't want Cucumber Unicode support
+require 'cucumber/formatter/unicode'
+
+# Comment out the next line if you don't want transactions to
+# open/roll back around each scenario
 Cucumber::Rails.use_transactional_fixtures
 
+# Comment out the next line if you want Rails' own error handling
+# (e.g. rescue_action_in_public / rescue_responses / rescue_from)
+Cucumber::Rails.bypass_rescue
+
 require 'webrat'
+require 'cucumber/webrat/table_locator' # Lets you do table.diff!(table_at('#my_table').to_a)
 
 Webrat.configure do |config|
   config.mode = :rails
 end
 
-# Comment out the next two lines if you're not using RSpec's matchers (should / should_not) in your steps.
 require 'cucumber/rails/rspec'
 require 'webrat/core/matchers'
