52efd6a495243c653636e5c4434f1697a965f136
Jesse Crockett <jess@home.(none)>
2009-08-31 17:50:08 -0500

    touchup social news feed

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,7 +4,7 @@ module HomeHelper
     mutual_friendships(user).each do |m|
       # new blitzes
       m.friend.blitzes.session.collect! do |b|
-        list << b if b.votes.find_by_user_id(user).nil?
+        list << b if !b.votes.exists?(:user_id => user)
       end
       # won blitzes
       m.friend.blitzes.awarded.each do |b|
@@ -12,7 +12,8 @@ module HomeHelper
       end
       # joined groups
       m.friend.groups.reverse.collect! do |g|
-        list << g.memberships.find_by_user_id(m.friend)
+        list << g.memberships.find_by_user_id(m.friend) # unless g.memberships.
+                                                      #exists?(:user_id => user)
       end
     end
     @list = list.sort {|a,b| b.updated_at <=> a.updated_at }.
@@ -20,9 +21,13 @@ module HomeHelper
   end
 
   def group_message(membership)
-    if membership.class.name == 'Membership'
+    if membership.class.name.eql?('Membership')
+      if membership.role.eql?('creator')
+      'created'
+        # TODO add more logic for promotions etc
+      else
       'joined'
-      # TODO add more logic for creating groups, promotions etc
+      end
     else
       nil
     end
@@ -30,9 +35,13 @@ module HomeHelper
 
   def grant_message(grant)
     if grant.awarded and grant.final
-      'won'
+      'won!'
     elsif !grant.final
-      'created'
+      if grant.class.name.eql?('Blitz')
+        'blitzes'
+      else
+        'writes'
+      end
     else
       nil
     end
@@ -40,24 +49,24 @@ module HomeHelper
 
   def activity_link(instance, photo = false)
     if photo
-      case instance.class.name.downcase
-        when 'membership'
+      case instance.class.name
+        when 'Membership'
           link_to user_defined_image(instance.group, :thumb,
             :alt => instance.group.name), group_path(instance.group)
-        when 'grant'
+        when 'Grant'
           link_to user_defined_image(instance, :thumb, :alt => instance.name),
             group_grant_path(instance.group, instance)
-        when 'blitz'
+        when 'Blitz'
           # link_to 'Blitz Icon', blitz_path(instance)
           link_to image_tag('/images/icons/blitz_32.png'), blitz_path(instance)
       end
     else
-      case instance.class.name.downcase
-        when 'membership'
+      case instance.class.name
+        when 'Membership'
           link_to(instance.group.name, group_path(instance.group))
-        when 'blitz'
+        when 'Blitz'
           link_to(instance.name, blitz_path(instance))
-        when 'grant'
+        when 'Grant'
           link_to(instance.name, group_grant_path(instance.group, instance))
       end
     end
