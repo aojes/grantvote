24933c151f5ee5570183d538026081da420b5cee
Jesse Crockett <jess@home.(none)>
2009-08-31 15:46:15 -0500

    fix payment token generation, yeah baby :-)

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -13,7 +13,7 @@ class PaymentsController < ApplicationController
 
     request = Remit::InstallPaymentInstruction::Request.new(
       :payment_instruction  => "MyRole == 'Caller' orSay 'Role does not match';",
-      :caller_reference     => Time.now.to_i.to_s,
+      :caller_reference     => "#{Time.now.to_i + rand(1000)}",
       :token_friendly_name  => "Grantvote Caller Token",
       :token_type           => "SingleUse"
     )
@@ -23,7 +23,7 @@ class PaymentsController < ApplicationController
 
     request = Remit::InstallPaymentInstruction::Request.new(
       :payment_instruction  => "MyRole == 'Recipient' orSay 'Role does not match';",
-      :caller_reference     => Time.now.to_i.to_s,
+      :caller_reference     => "#{Time.now.to_i + rand(1000)}",
       :token_friendly_name  => "Grantvote Payment Receipt",
       :token_type           => "SingleUse"
     )
